db99a5ad9eaeaf635cc348e8989dc9a55443efb6
jo <jo@t450.fritz.box>
2016-09-18 20:58:58 +0200

    insert configs

--- livemonitoring-ws-transmitter/src/main/java/de/tiq/solutions/livemon/app/main/LmConfigs.java
+++ livemonitoring-ws-transmitter/src/main/java/de/tiq/solutions/livemon/app/main/LmConfigs.java
@@ -1,36 +1,89 @@
 package de.tiq.solutions.livemon.app.main;
 
 import java.io.File;
+import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 
 public class LmConfigs {
-	private static class LmConfig{
-		private 
-		
+	@XmlRootElement(name = "settings")
+	private static class Settings {
+		 
+		private List<Setting> setting;
+		 
+		public List<Setting> getSetting() {
+			return setting;
+		}
+		public void setSetting(List<Setting> setting) {
+			this.setting = setting;
+		}
+		 
+		public static class Setting {
+			private String name;
+			private String value;
+			private String desc;
+
+			public String getName() {
+				return name;
+			}
+
+			public void setName(String name) {
+				this.name = name;
+			}
+
+			public String getValue() {
+				return value;
+			}
+
+			public void setValue(String value) {
+				this.value = value;
+			}
+
+			public String getDesc() {
+				return desc;
+			}
+
+			public void setDesc(String desc) {
+				this.desc = desc;
+			}
+
+		}
+
+
 	}
-	private int s=456;
-	public String getConfig(){
-		System.out.println("nachdem es gebaut wurde "+s);
+
+	private int s = 456;
+
+	public String getConfig() {
+		System.out.println("nachdem es gebaut wurde " + s);
 		return "hier kommts";
 	}
+
 	{
-		//versuch die config datei zu lesen wenn nicht dann ist die Instanz der Config klasse null
+		// versuch die config datei zu lesen wenn nicht dann ist die Instanz der
+		// Config klasse null
 		try {
-			JAXBContext jc = JAXBContext.newInstance(LmConfig.class);
-			jc.createUnmarshaller().unmarshal(new File("../config/lm_config.xml"));
+			JAXBContext jc = JAXBContext.newInstance(Settings.class);
+			File file = new File("../config/lm_config.xml");
+			System.out.println(file.getAbsolutePath());
+			Settings settings = (Settings)jc.createUnmarshaller().unmarshal(file);
+ 		System.out.println(settings.getSetting().size());
 		} catch (JAXBException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		System.out.println("sdf "+s);
-		LmConfigs.this.s=1111;
-		System.out.println("klasse wird genutzt "+s+  "  ");
+		System.out.println("sdf " + s);
+		LmConfigs.this.s = 1111;
+		System.out.println("klasse wird genutzt " + s + "  ");
 	}
+
 	private LmConfigs() {
-		
+
 	}
 
 	private static LmConfigs instance;
